2c69df9c40323dfa023766a3b74e0de14aaf3737
Marko Ivanovic <marko.ivanovic.dev@gmail.com>
2021-09-23 21:17:59 +0200

    random quote api call

--- public/process.php
+++ public/process.php
@@ -1,17 +1,33 @@
 <?php
-ini_set('display_errors',1);
+header('Content-Type: application/json');
 
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\QuotesController;
 
+$https = $_SERVER['HTTPS'] ?? null;
+$host = $_SERVER['HTTP_HOST'];
+$req_uri = $_SERVER['REQUEST_URI'];
+
 $controller = new QuotesController;
-if ($_GET['rand']){
+
+if (isset($_GET['rand'])){
     echo $controller->fetchRandomQuote();
+    return;
 }
+if (isset($_GET['next'])){
+    echo $controller->fetchNextQuote($_GET['id']);
+    return;
+}
+
 // if ($_GET['prev']){
 //     echo $controller->fetchPreviousQuote($_GET['id']);
 // }
-if ($_GET['next']){
-    echo $controller->fetchNextQuote($_GET['id']);
+// $url = ($https ? "https" : "http") . "://$host$req_uri";
+
+if ($req_uri === '/quote/random') {
+    echo $controller->fetchRandomQuote();
+    return;
+} else {
+    echo json_encode(new stdClass, JSON_PRETTY_PRINT);
 }
